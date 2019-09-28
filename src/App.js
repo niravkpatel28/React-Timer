9e181ef7d74cca52f94f98e3de5bb90ce2127eaa
niravkpatel28 <niravkpatel28@gmail.com>
2019-09-28 13:55:39 +0530

    Start and Pause Buttons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Timer name="Nirav"/>
+      <Timer name="Timer App"/>
       </header>
     </div>
   );
--- src/components/common/Timer.js
+++ src/components/common/Timer.js
@@ -3,31 +3,54 @@ import React from 'react'
 
 class Timer extends React.Component {
 
-    constructor(props){
-        super(props);
-        setInterval(this.ticker, 1000);
-    }
+    // constructor(props){
+    //     super(props);
+    //     // setInterval(this.ticker, 1000);
+    // }
     // defines internal state of the object 
     state={
         seconds: 0,
-        minute:0
+        timerStarted: false,
+        timerID: null
     }
 
     ticker =()=>{
         this.setState(
             {
-                // seconds:Math.round((this.state.seconds+1)%60),
                 seconds: this.state.seconds+1
-                //minute: Math.round((this.state.seconds)/60)
             });
             
     }
 
     //click event handler 
-
-    
-    render(){
+    startTimer = ()=>{
+        let timerId
+        if(!this.state.timerStarted){
+            timerId = setInterval(this.ticker,1000);
+            this.setState(
+                {
+                    timerStarted:true,
+                    timerID: timerId // timerId is set locally
+                })
+             console.log('timer on');
+             console.log(this.state.timerID);
+        }else{
+            console.log('Timer Already On');
+        }
         
+    }
+    
+    pauseTimer = ()=>{
+        //clear interval with timer id
+        if(this.state.timerStarted){
+            clearInterval(this.state.timerID);
+            this.setState({
+                timerStarted:false,
+                timerID:null
+            })
+        }
+    }
+    render(){
         return (
             <div>
                 <h2> Welcome {this.props.name}</h2>
@@ -35,7 +58,10 @@ class Timer extends React.Component {
                 <p> Minute : Seconds </p>
                 <p> {Math.floor((this.state.seconds)/60)}  :  {this.state.seconds%60} </p>
                 {/* <p>Total Seconds : {this.state.seconds}</p> */}
-               
+                <button onClick={this.startTimer}> Start Timer </button>
+                <button onClick={this.pauseTimer}> Pause Timer </button>
+
+                
             </div> 
  
         )
