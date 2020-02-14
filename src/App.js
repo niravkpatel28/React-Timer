073ab7ef59094f2d19b81095bb18bf2c2c112013
niravkpatel28 <niravkpatel28@gmail.com>
2020-02-14 17:56:59 +0530

    Code sandbox changes
    This code change is made using code sandbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Timer from '../src/components/common/Timer';
+import "./App.css";
+import Timer from "../src/components/common/Timer";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Timer name="Timer App"/>
+        <Timer name="Timer App" />
+        <h1> Change made in sandbox</h1>
       </header>
     </div>
   );
