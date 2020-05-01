a29a2cd8cec6713ca760e68256a87d6515b60c17
Nirav Patel <niravkpatel28@gmail.com>
2020-05-01 20:13:48 +0530

    Updates

---
+++ precache-manifest.7ec555f18234ee673308d0fc6c149df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f434788e7c2e779d7cebda5e6b45e74",
+    "url": "/React-Timer/index.html"
+  },
+  {
+    "revision": "deb1a3138b4178aabbe3",
+    "url": "/React-Timer/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ca5730f8b830c37bb14",
+    "url": "/React-Timer/static/js/2.b7696b43.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/React-Timer/static/js/2.b7696b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb1a3138b4178aabbe3",
+    "url": "/React-Timer/static/js/main.826f2f85.chunk.js"
+  },
+  {
+    "revision": "c2aaf7c2bbdded2f0ab1",
+    "url": "/React-Timer/static/js/runtime-main.b9257886.js"
+  }
+]);
